3f09fe89499afc958305132f0a2ae6dda129c9fe
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-04-12 16:17:16 +0200

    list style

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP ToDo List JSON</title>
 
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- Axios -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
@@ -15,6 +21,22 @@
 </head>
 
 <body>
+    <div class="to-do-list mx-auto mt-5">
+        <h1 class="text-center mb-4">Todo List</h1>
+        <ul class="list-group list-group-numbered">
+            <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                <span class="ms-1 me-auto">Ciao</span>
+                <span class="badge bg-danger" role="button">
+                    <i class="fa-solid fa-trash"></i>
+                </span>
+            </li>
+        </ul>
+
+        <div class="input-group mt-3">
+            <input type="text" class="form-control" placeholder="Inserisci un elemento" aria-label="Inserisci un elemento" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Inserisci</button>
+        </div>
+    </div>
     <!-- Vue JS -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </body>
